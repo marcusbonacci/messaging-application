75d90279b5b65e4d4b1c7d5e8e6cece462412553
marcusbonacci <marcusbonacci+github@gmail.com>
2026-04-04 22:05:20 -0700

    Move: /app

---
+++ backend/app/api/users/routes.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from uuid import UUID
+
+from db import postgres
+from psycopg.rows import dict_row
+
+router = router = APIRouter(prefix="/users")
+
+@router.get("/{user_id}")
+async def get_user(user_id: UUID):
+    query = "SELECT * FROM users WHERE user_id = %s"
+    async with postgres.get_conn() as conn:
+        async with conn.cursor(row_factory=dict_row) as cur:
+            await cur.execute(query, (str(user_id),))
+            row = await cur.fetchone()
+            return row
+
+@router.get("/")
+async def get_users(limit: int = 10, skip: int = 0):
+    query = "SELECT * FROM users LIMIT %s OFFSET %s"
+    async with postgres.get_conn() as conn:
+        async with conn.cursor(row_factory=dict_row) as cur:
+            await cur.execute(query, (limit, skip))
+            rows = await cur.fetchall()
+            return {"users": rows}
