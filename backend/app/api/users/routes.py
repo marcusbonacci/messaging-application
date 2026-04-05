43a53588e8e33b5cdcfd9906bf6ae57f0c494353
marcusbonacci <marcusbonacci+github@gmail.com>
2026-04-04 22:16:36 -0700

    Pseudo: users_router, put and delete code

--- backend/app/api/users/routes.py
+++ backend/app/api/users/routes.py
@@ -23,3 +23,19 @@ async def get_users(limit: int = 10, skip: int = 0):
             await cur.execute(query, (limit, skip))
             rows = await cur.fetchall()
             return {"users": rows}
+    
+# router.delete("/{user_id}")
+# async def delete_user(user_id: UUID):
+#     query = "SELECT * FROM users WHERE user_id = %s"
+#     async with postgres.get_conn() as conn:
+#         async with conn.cursor(row_factory=dict_row) as cur:
+#             await cur.execute(query, (str(user_id),))
+#             return {"users": "deleted"}
+        
+# router.put("/{user_id}")
+# async def update_user(user_id: UUID):
+#     query = "UPDATE * from users WHERE user_id = %s"
+#     async with postgres.get_conn() as conn:
+#         async with conn.cursor(query, row_factory=dict_row) as cur:
+#             await cur.execute(query, (str(user_id),))
+#             return {"TBD"}
