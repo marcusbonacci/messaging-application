6400be89028847740c8c9e7264f7d95b1357dce2
marcusbonacci <marcusbonacci+github@gmail.com>
2026-04-05 19:00:47 -0700

    Mod: SQL Cleanup

--- fast-api/app/api/users/routes.py
+++ fast-api/app/api/users/routes.py
@@ -55,13 +55,12 @@ async def get_friends(user_id: UUID, limit: int = 10, skip: int = 0):
     SELECT
         CASE WHEN user_a_id = %(user_id)s THEN user_b_id ELSE user_a_id END AS friend_id,
         created_at, updated_at
-    FROM 
-        friends
+    FROM friends
     WHERE %(user_id)s IN (user_a_id, user_b_id)
-    LIMIT %(limit)s
-    OFFSET %(skip)s;
+    LIMIT %(limit)s OFFSET %(skip)s;
     """
     params = {"user_id": user_id, "limit": limit, "skip": skip}
+
     async with postgres.get_conn() as conn:
         async with conn.cursor(row_factory=dict_row) as cur:
             await cur.execute(query, params)
